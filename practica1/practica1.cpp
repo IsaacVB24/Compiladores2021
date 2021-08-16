ad078db161307462c35eaf00de97f7601c40bc64
IsaacVB24 <55108168+IsaacVB24@users.noreply.github.com>
2021-08-16 17:46:28 -0500

    Add files via upload

---
+++ practica1/practica1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class AutomataFinitoDeterminista{
+	private:
+		int numeroNodos;
+		int conexionesPorNodo = numeroNodos - 1;
+		int inicio;
+		int nodoFinal;
+		int nodoActual;
+		int *nodoSiguiente;
+	public:
+		void irANodo();
+		void comprobarTransicion();
+};
+
+void AutomataFinitoDeterminista::irANodo(){
+	
+}
+
+void AutomataFinitoDeterminista::comprobarTransicion(){
+	
+}
+
+class AutomataFinitoNoDeterminista{
+	private:
+		int numeroNodos;
+		int conexionesPorNodo = numeroNodos - 1;
+		int inicio;
+		int nodoFinal;
+		int nodoActual;
+		int *nodoSiguiente;
+	public:
+		void irANodo();
+};
+
+void AutomataFinitoNoDeterminista::irANodo(){
+	
+}
+
+int main(){
+	AutomataFinitoDeterminista nuevoAFD;
+	AutomataFinitoNoDeterminista nuevoAFND;
+}
