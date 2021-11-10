e98089147ece3abc4d7414c35f64dbb40a8b8dcf
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2021-11-10 16:04:17 -0600

    Add files via upload

---
+++ Practica2/afn.cpp
@@ -0,0 +1,8 @@
+#include "afn.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+}
---
+++ Practica2/afn.hpp
@@ -0,0 +1,16 @@
+
+
+
+class Automata{
+	int cantEstados, numEdoAct, edoIni, edoFin;
+	Automata(){
+		cantEstados = 0;
+		numEdoAct = 0;
+		edoIni = 0;
+		edoFin = 0;
+	}
+public:
+			void transicion(){
+				
+			}
+};
