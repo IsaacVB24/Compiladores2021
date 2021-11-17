1736393feec9e6bcafe3dccc62365552a3be619a
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2021-11-16 22:28:03 -0600

    Add files via upload

--- Practica2/ExpresionRegular.hpp
+++ Practica2/ExpresionRegular.hpp
@@ -8,10 +8,11 @@ class ExpresionRegular{
 	public:
 		std::vector<char> operandos, operadores;
 		int tamER, auxiliarInsercionOperador=0;
+		char epsilon = 'E';
 		std::vector<char> listaOperadores = {'(', ')', '+', '|'};
 		ExpresionRegular(string expresion){
 			for(int i=0; i<expresion.length(); i++){
-				for(int j=0; j<3; j++){
+				for(int j=0; j<=3; j++){
 					if(expresion[i] == listaOperadores[j]){
 						operadores.push_back(listaOperadores[j]);
 						auxiliarInsercionOperador++;
@@ -21,7 +22,6 @@ class ExpresionRegular{
 			for(int k=0; k<=(auxiliarInsercionOperador-1); k++){
 				cout << operadores[k];
 			}
-			cout << expresion.length();
 			operadores = {};
 			operandos = {};
 			tamER = expresion.length();
