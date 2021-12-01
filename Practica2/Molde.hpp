1d46173d772ae05c518deeca6eb5481c811d4f01
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2021-11-30 23:22:21 -0600

    Add files via upload

--- Practica2/Molde.hpp
+++ Practica2/Molde.hpp
@@ -5,6 +5,8 @@ class Molde{
 	int cantEstados, *edoOrigen, edoDestino;
 	Molde(){
 		cantEstados = 0;
+		edoOrigen = 0;
+		edoDestino = 0;
 	}
 	public:
 		void transicionEpsilon(int *edoOrigen, int edoDestino){
