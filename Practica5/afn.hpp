918de7d82c8733482ef8ccb2cbd8b58a680094da
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2021-12-01 16:30:50 -0600

    Add files via upload

---
+++ Practica5/afn.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+void funcionDeTransicion(char *estadosSiguientes);
+void agregarEstado();
+//AFN representado por tabla de transiciones
+class AFN{
+	public:
+		std::vector<char> conjuntoDeEstados;
+		std::vector<char> alfabetoDeEntrada;
+		std::vector<char> estadosSiguientes;
+		char estadoInicial;
+		std::vector<char> estadosFinales;
+		std::vector<bool> yaEstaEn;
+		std::vector<char> estadosAnt;
+		std::vector<char> estadosNuev;
+		//char mover[][];
+		int numeroDeEstados;
+		AFN(int numeroDeEstados){
+			conjuntoDeEstados = {};
+			alfabetoDeEntrada = {};
+			estadosSiguientes = {};
+			estadoInicial = 'a';
+			estadosFinales = {};
+			for(int i=0; i<numeroDeEstados; i++){
+				yaEstaEn[i] = {false};
+			}
+			std::cout << "El número de estados es: ";
+		}
+};
