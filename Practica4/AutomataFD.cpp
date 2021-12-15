b9dd9982b7d0784b22725f5d87ad7e27f444ed23
Isaac Victoria <55108168+IsaacVB24@users.noreply.github.com>
2021-12-15 17:00:42 -0600

    Add files via upload

--- Practica4/AutomataFD.cpp
+++ Practica4/AutomataFD.cpp
@@ -4,4 +4,21 @@
 using namespace std;
 
 int main(){
+	FILE *fptr;
+	fptr = fopen("prueba.dot", "wb");
+	string er;
+	cout << "Para hacer uso de \"|\" y \"*\", use paréntesis en ambos lados (Ejemplo: (ab|f(a*)). \n\nIngrese la expresión regular: ";
+	cin >> er;
+	cout << er << endl << endl;
+	fprintf(fptr, "digraph AFN{\nrankdir = LR;\n");
+	string prueba="h(osssl(daqws)asdcsdc)";	
+	
+	
+	concatenacion(er, fptr, er.length());
+	//seleccion(prueba);
+	fprintf(fptr, "\n}");
+	ESTADO nuevo;
+	fclose(fptr);
+	system("dot.exe -Tpng prueba.dot -o grafo.png");
+	return 0;
 }
